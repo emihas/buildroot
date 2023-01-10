7e02c6c79ad780627b6f4d08ec70192f9c8277b4
emihas <emihas@addiva.se>
2023-01-10 16:00:33 +0100

    Use post-build script to create target start script

--- board/qemu/x86_64/post-build.sh
+++ board/qemu/x86_64/post-build.sh
@@ -9,3 +9,10 @@ if [ -e ${TARGET_DIR}/etc/inittab ]; then
 	sed -i '/GENERIC_SERIAL/a\
 tty1::respawn:/sbin/getty -L  tty1 0 vt100 # QEMU graphical window' ${TARGET_DIR}/etc/inittab
 fi
+
+# Start tetris....
+echo "Creating auto-start script"
+cat <<EOF > ${TARGET_DIR}/etc/init.d/S50tetris
+tetris
+EOF
+chmod 755 ${TARGET_DIR}/etc/init.d/S50tetris
